a211c30c0b00a5e785b7ae5f63b228e13bcddcd5
pjoeoh <thejoeoh@gmail.com>
2024-12-08 12:34:12 -0800

    update

---
+++ demo/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  demo
+//
+//  Created by USER on 12/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by USER on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
